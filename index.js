90e73423f3ea3760fb315936bef63722116212b6
Ishaan Chamoli <email@ishaanchamoli.com>
2024-06-26 19:54:35 +0530

    Testing

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-//Starting now!
+import bodyParser from "body-parser";
+import express from "express";
+import axios from "axios";
+import pg from "pg";
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "bookwars",
+    password: "L!gtctwwpo@256"
+
+})
+
+
+
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static("public"));
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server runing on port ${port}`);
+});
