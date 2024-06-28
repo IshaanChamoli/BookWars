8cc3003a0c5c1a7fc7327a756f761461c7e31ce2
Ishaan Chamoli <email@ishaanchamoli.com>
2024-06-28 17:09:20 +0530

    Add auto bookcover image

--- index.js
+++ index.js
@@ -37,10 +37,13 @@ app.get("/", async (req, res) => {
     for (let i = 0; i < readers.length; i++) {
         readersArray.push(readers[i].name)
     }
+    
 
     //getting 2d array of each readers books
     var books2DArray = []
     var bookRatings2DArray = []
+    var booksOLID2DArray = []
+    console.log("Started loading book covers!");
     for (let i = 0; i < readers.length; i++) {
         if (ratingBased) {
             var datatwo = await db.query("SELECT * FROM books WHERE user_id = $1 ORDER BY rating DESC", [readers[i].id]);
@@ -50,18 +53,30 @@ app.get("/", async (req, res) => {
         var singleBooks = datatwo.rows;
         var singleBooksArray = [];
         var singleBookRatingsArray = [];
+        var singleBooksOLIDArray = [];
         for (let j = 0; j < singleBooks.length; j++) {
             singleBooksArray.push(singleBooks[j].book_name);
+            var olid = "";
+            try {
+                const result = await axios.get(`https://openlibrary.org/search.json?q=${singleBooks[j].book_name}`)
+                olid = result.data.docs[0].cover_edition_key;
+            } catch {
+                olid = "NA";
+            }
             singleBookRatingsArray.push(singleBooks[j].rating);
+            singleBooksOLIDArray.push(olid);
         } 
         books2DArray.push(singleBooksArray);
         bookRatings2DArray.push(singleBookRatingsArray);
+        booksOLID2DArray.push(singleBooksOLIDArray);
     }
+    console.log("Book covers loaded!")
     // FINAL ARRAYS 
     res.render("index.ejs", {
         readers: readersArray,
         books: books2DArray,
-        ratings: bookRatings2DArray
+        ratings: bookRatings2DArray,
+        olids: booksOLID2DArray
     });
 });
 
@@ -82,8 +97,6 @@ app.post("/added", async (req, res) => {
 app.post("/delete", async (req, res) => {
     var data = await db.query("SELECT id FROM users WHERE name = $1", [req.body.whichUser]);
     var id = data.rows[0].id;
-    console.log(id);
-    console.log(req.body.whichBook);
     await db.query("DELETE FROM books WHERE book_name = $1 AND user_id = $2", [req.body.whichBook, id]);
     res.redirect("/")
 })
