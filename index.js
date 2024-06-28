19d4686829b49ba4b6f2de3aff26e8a2435b361a
Ishaan Chamoli <email@ishaanchamoli.com>
2024-06-28 17:32:01 +0530

    Load book cover images asynchronously so that page loads faster, even if book covers take long to load through API

--- index.js
+++ index.js
@@ -31,52 +31,38 @@ app.post("/sorted", (req, res) => {
 
 app.get("/", async (req, res) => {
     //getting array of all readers
-    var readersArray = []
+    var readersArray = [];
     var data = await db.query("Select * FROM users");
-    var readers = data.rows
+    var readers = data.rows;
     for (let i = 0; i < readers.length; i++) {
-        readersArray.push(readers[i].name)
+        readersArray.push(readers[i].name);
     }
-    
 
     //getting 2d array of each readers books
-    var books2DArray = []
-    var bookRatings2DArray = []
-    var booksOLID2DArray = []
-    console.log("Started loading book covers!");
+    var books2DArray = [];
+    var bookRatings2DArray = [];
     for (let i = 0; i < readers.length; i++) {
+        var datatwo;
         if (ratingBased) {
-            var datatwo = await db.query("SELECT * FROM books WHERE user_id = $1 ORDER BY rating DESC", [readers[i].id]);
+            datatwo = await db.query("SELECT * FROM books WHERE user_id = $1 ORDER BY rating DESC", [readers[i].id]);
         } else {
-            var datatwo = await db.query("SELECT * FROM books WHERE user_id = $1 ORDER BY id DESC", [readers[i].id]);
+            datatwo = await db.query("SELECT * FROM books WHERE user_id = $1 ORDER BY id DESC", [readers[i].id]);
         }
         var singleBooks = datatwo.rows;
         var singleBooksArray = [];
         var singleBookRatingsArray = [];
-        var singleBooksOLIDArray = [];
         for (let j = 0; j < singleBooks.length; j++) {
             singleBooksArray.push(singleBooks[j].book_name);
-            var olid = "";
-            try {
-                const result = await axios.get(`https://openlibrary.org/search.json?q=${singleBooks[j].book_name}`)
-                olid = result.data.docs[0].cover_edition_key;
-            } catch {
-                olid = "NA";
-            }
             singleBookRatingsArray.push(singleBooks[j].rating);
-            singleBooksOLIDArray.push(olid);
-        } 
+        }
         books2DArray.push(singleBooksArray);
         bookRatings2DArray.push(singleBookRatingsArray);
-        booksOLID2DArray.push(singleBooksOLIDArray);
     }
-    console.log("Book covers loaded!")
-    // FINAL ARRAYS 
+
     res.render("index.ejs", {
         readers: readersArray,
         books: books2DArray,
-        ratings: bookRatings2DArray,
-        olids: booksOLID2DArray
+        ratings: bookRatings2DArray
     });
 });
 
